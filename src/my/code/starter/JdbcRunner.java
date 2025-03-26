425acb8e2139ffef5c4e75fdcd7c2cfb8c94018e
Maksym <MiToSSH@gmail.com>
2025-03-26 21:03:58 +0200

    003 Connecting to databases

--- src/my/code/starter/JdbcRunner.java
+++ src/my/code/starter/JdbcRunner.java
@@ -1,7 +1,15 @@
 package my.code.starter;
 
+import my.code.starter.util.ConnectionManager;
+
+import java.sql.Driver;
+import java.sql.SQLException;
+
 public class JdbcRunner {
-    public static void main(String[] args) {
-        System.out.println("Starting");
+    public static void main(String[] args) throws SQLException {
+        Class<Driver> driverClass = Driver.class;
+        try (var connection = ConnectionManager.open()) {
+            System.out.println(connection.getTransactionIsolation());
+        }
     }
 }
